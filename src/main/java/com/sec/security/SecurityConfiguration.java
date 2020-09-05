17dceb23b1bf27b3de1eff3a1532119825abc884
bptech-beta <bptech.beta@gmail.com>
2020-09-05 22:13:49 +0200

    favicon, logout button added

--- src/main/java/com/sec/security/SecurityConfiguration.java
+++ src/main/java/com/sec/security/SecurityConfiguration.java
@@ -30,6 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
+            .antMatchers( "/favicon.ico").permitAll()
            // .antMatchers("/user").hasAnyRole("USER")
             .antMatchers("/").hasAnyRole("ADMIN" , "USER")
             .and().formLogin();
